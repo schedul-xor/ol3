8dd8497b3367a6ae9eccb0bd84ac9019c30a3157
schedul-xor <schedul.xor@gmail.com>
2014-01-26 16:48:31 +0900

    Fixed require order

--- examples/vector-cubic-bezier.js
+++ examples/vector-cubic-bezier.js
@@ -9,8 +9,8 @@ goog.require('ol.geom.LineString');
 goog.require('ol.geom.Point');
 goog.require('ol.layer.Vector');
 goog.require('ol.source.Vector');
-goog.require('ol.style.Fill');
 goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
 goog.require('ol.style.Stroke');
 goog.require('ol.style.Style');
 
